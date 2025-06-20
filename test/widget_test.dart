adec5d05602c9d59efd41bb68cb57512b779aff9
Ben <wcat06821@gmail.com>
2025-06-20 22:31:33 +0200

    fixed/ui-main page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hebrew_voice_assistant/main.dart';
+import 'package:Hebrew_Voice_Assistant/screens/home_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('HomeScreen displays welcome message', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+    expect(find.text('Welcome to Hebrew Voice Assistant'), findsOneWidget);
   });
 }
